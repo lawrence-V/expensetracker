c4e17d767de570e0a5b38d850fedb43906c6c8db
Lawrence <lawrencevaldez027@gmail.com>
2025-09-19 21:48:41 +0800

    frontend initial

---
+++ frontend/app/layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <UContainer class="border-default pt-10">
+      <AppHeader />
+      <slot />
+      <AppFooter />
+    </UContainer>
+  </div>
+</template>
---
+++ frontend/app/pages/test.vue
@@ -0,0 +1,6 @@
+<template>
+  <UPageHero
+    title="Ultimate Vue UI library"
+    description="A Nuxt/Vue-integrated UI library providing a rich set of fully-styled, accessible and highly customizable components for building modern web applications."
+  />
+</template>
